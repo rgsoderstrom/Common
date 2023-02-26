3e12abf9898e36da0928b9e4a7ac46a4ce02c1e0
Randy Soderstrom <rgsoderstrom@gmail.com>
2023-02-26 07:56:01 -0800

    Add NameValuePairs class

---
+++ NameValuePair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class NameValuePair
+    {
+        public string Name;
+        public object Value;
+
+        public NameValuePair (string n, object v)
+        {
+            Name = n;
+            Value = v;
+        }
+
+        public override string ToString ()
+        {
+            return Name + " " + Value.GetType ().Name.ToString () + " " + Value.ToString ();
+        }
+    }
+}
